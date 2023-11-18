d2a55742a4c71d953ed258aabd3dd8e85b3c8483
Islam walied <islam.walied96@gmail.com>
2023-11-18 23:11:02 +0200

    adding database to the project

---
+++ database/migrations/2023_11_18_205211_create_therapists_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('therapists', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('specialist');
+            $table->string('location');
+            $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->integer('ExperienceYear')->default(0);
+            $table->string('certification')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('therapists');
+    }
+};
